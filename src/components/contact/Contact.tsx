b8f5f0e7a23535fdc475ea6787a03172d20530df
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-04-24 15:13:12 +0300

    refactoring code in Contact.tsx

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faMapLocationDot} from "@fortawesome/free-solid-svg-icons";
+import {useTranslation} from "react-i18next";
 
 export const Contact = () => {
     const email = 'oleg.ostrovski@gmail.com'
+    const {t} = useTranslation('contact')
 
     return (
         <section id="contact" className="contact">
             <div className="container">
                 <div className="contact__content">
                     <div className="contact__title">
-                        <p>contact</p>
-                        <h3>Write to me here ... ✍🏾</h3>
+                        <p>{t('контакты')}</p>
+                        <h3>{t('Напиши мне здесь')} ... ✍🏾</h3>
                     </div>
+
                     <div className="contact__icons">
                         <div className="contact__icon-box">
-                                <span>
-                                    <FontAwesomeIcon icon={faMapLocationDot} size={'3x'}/>
-                                </span>
+                            <span>
+                                <FontAwesomeIcon icon={faMapLocationDot} size={'3x'}/>
+                            </span>
+
                             <div className="contact__info">
-                                <h3>Location</h3>
-                                <p>Minsk, Belarus</p>
+                                <h3>{t('Местонахождение')}</h3>
+                                <p>{t('Минск, Беларусь')}</p>
                             </div>
                         </div>
+
                         <div className="contact__icon-box">
-                                <span>
-                                    <FontAwesomeIcon icon={faEnvelope} size={'3x'}/>
-                                </span>
+                            <span>
+                                <FontAwesomeIcon icon={faEnvelope} size={'3x'}/>
+                            </span>
+
                             <div className="contact__info">
-                                <h3>Mail</h3>
+                                <h3>{t('Почта')}</h3>
                                 <a href={`mailto:${email}`}>{email}</a>
                             </div>
                         </div>
